186319e72e406911793984434cef1385bdd9999a
Ngọc Long Nguyễn <ngoclong1282001@gmail.com>
2021-05-18 23:54:29 +0700

    rename

--- modules/pre_process.py
+++ modules/pre_process.py
@@ -11,7 +11,7 @@ def pre_process_yolo(img):
     img_ = img_.unsqueeze(0)
     return img_
 
-def pro_process_openvino(x):
+def pre_process_openvino(x):
     x = cv2.resize(x, (120, 120))
     x = torch.from_numpy(x).cuda()
     x = x.to(dtype=torch.float)
