914334c615454c184c96c9ec877594d9114c89c5
209sontung <209tungns@gmail.com>
2021-05-18 19:59:18 +0700

    add to utils

--- modules/pre_process.py
+++ modules/pre_process.py
@@ -1,8 +1,15 @@
 import torch
 import cv2
+from utils.torch_utils import select_device
 
 def pre_process_yolo(img):
-    pass
+    img_ = img[..., ::-1].copy()
+    img_ = torch.from_numpy(img_).to(select_device('0'))
+    img_ = img_.half()
+    img_ /= 255.0
+    img_ = img_.permute(2, 0, 1)
+    img_ = img_.unsqueeze(0)
+    return img_
 
 def pro_process_openvino(x):
     x = cv2.resize(x, (120, 120))
