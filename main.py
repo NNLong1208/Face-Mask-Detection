4c996feae2d6eb754524ea4e8ea5c63cb7935203
nguye <nguyenngoclong1282001@gmail.com>
2021-06-28 02:17:49 +0700

    add thred

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import time
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--camera', type=int, default=0, help='id of camera or video')
+    parser.add_argument('--thred_yolo', type=int, default=0.47, help='thred yolo')
+    parser.add_argument('--thred_dis', type=int, default=3, help='thred distance box and hand')
     opt = parser.parse_args()
     MaskDetection = MaskDetection()
     MaskDetection.prepare()
@@ -14,7 +16,7 @@ if __name__ == '__main__':
         _, img = camera.read()
         img = cv2.resize(img, (640, 480))
         try:
-            pre = MaskDetection.detection(img)
+            pre = MaskDetection.detection(img, opt.thred_yolo, opt.thred_dis)
             img = MaskDetection.draw(img)
         except:
             pass
